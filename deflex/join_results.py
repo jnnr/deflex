6576e5e3edc140f1420e666e85c03784cb7d4c68
jnnr <32454596+jnnr@users.noreply.github.com>
2019-11-08 12:06:35 +0100

    Add script to join results

---
+++ deflex/join_results.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os
+
+sc_dir = '/home/jann/reegis/scenarios/deflex/2012/'
+dirs = [os.path.join(sc_dir, dir) for dir in os.listdir(sc_dir)
+        if dir.startswith('postproc')]
+
+all_f_res = pd.DataFrame()
+
+for dir in dirs:
+    f_res = pd.read_csv(os.path.join(dir, 'formatted_results.csv'), index_col=0)
+    all_f_res = pd.concat([all_f_res, f_res], ignore_index=True)
+
+
+
+sc_translation = {'2012_de02_electricity-only_100': 'Baseline battery costs|Full geographic coverage|Electricity only',
+                  '2012_de02_electricity-only_25': '25percent battery costs|Full geographic coverage|Electricity only',
+                  '2012_de02_electricity-only_50': '50percent battery costs|Full geographic coverage|Electricity only',
+                  '2012_de02_full_100': 'Baseline battery costs|Full geographic coverage|Electricity and Heat',
+                  '2012_de02_full_50': '50percent battery costs|Full geographic coverage|Electricity and Heat',
+                  '2012_de02_full_25': '25percent battery costs|Full geographic coverage|Electricity and Heat'}
+
+all_f_res = all_f_res.replace(sc_translation)
+all_f_res.to_csv(os.path.join(sc_dir, 'deflex_combined_results.csv'))
